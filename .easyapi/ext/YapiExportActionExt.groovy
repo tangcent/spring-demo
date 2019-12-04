3037f9be74a9000992b1a0f190592e41b4d4ce01
tangcent <sweettom@foxmail.com>
2019-12-04 18:14:03 +0800

    support old version of yapi

--- .easyapi/ext/YapiExportActionExt.groovy
+++ .easyapi/ext/YapiExportActionExt.groovy
@@ -1,44 +1,40 @@
+import com.google.inject.Inject
 import com.intellij.psi.*
-import com.itangcent.common.model.Request
-import com.itangcent.idea.plugin.api.export.ClassExporter
-import com.itangcent.idea.plugin.api.export.SpringRequestClassExporter
-import com.itangcent.idea.plugin.api.export.yapi.YapiClassExportRuleKeys
-import com.itangcent.idea.plugin.api.export.yapi.YapiRequestKitKt
+import com.itangcent.idea.plugin.api.export.yapi.YapiApiHelper
 import com.itangcent.idea.plugin.script.ActionExt
 import com.itangcent.idea.plugin.utils.KtHelper
+import com.itangcent.intellij.config.ConfigReader
 import com.itangcent.intellij.context.ActionContext
-
-import java.util.stream.Collectors
-import java.util.stream.Stream
 
 class YapiExportActionExt implements ActionExt {
 
     void init(ActionContext.ActionContextBuilder builder) {
 
-        builder.bind(ClassExporter.class, KtHelper.INSTANCE.ktFunction({
-            it.to(CustomClassExporter.class).in(com.google.inject.Singleton.class)
+        builder.bind(YapiApiHelper.class, KtHelper.INSTANCE.ktFunction({
+            it.to(OldYapiApiHelper.class).in(com.google.inject.Singleton.class)
             return null
         }))
-
     }
 
-    static class CustomClassExporter extends SpringRequestClassExporter {
+    static class OldYapiApiHelper extends YapiApiHelper {
 
-        void processCompleted(PsiMethod method, Request request) {
-            super.processCompleted(method, request)
+        @Inject
+        ConfigReader configReader;
 
-            String tags = ruleComputer.computer(YapiClassExportRuleKeys.TAG, method)
-            if (tags != null && !tags.isEmpty()) {
-                YapiRequestKitKt.setTags(request, Stream.of(tags.split("\n"))
-                        .map { it.trim() }
-                        .filter { !it.isEmpty() }
-                        .collect(Collectors.toList())
-                )
+        String findCat(String token, String name) {
+            return findConfig("catId[" + token + "][" + name + "]")
+        }
+
+        String getProjectIdByToken(String token) {
+            return findConfig("projectId[" + token + "]")
+        }
+
+        String findConfig(String name) {
+            String value = configReader.first(name)
+            if (value == null || value.isEmpty()) {
+                logger.info("please set: " + name)
             }
-
-            String status = ruleComputer.computer(YapiClassExportRuleKeys.STATUS, method)
-            logger.info(YapiRequestKitKt.class.toString())
-            YapiRequestKitKt.setStatus(request, status)
+            return value
         }
     }
 }
